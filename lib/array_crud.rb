3cced8e61089cfe31626b7283eb389beaafbfc95
diegojctorres <diegojctorres@gmail.com>
2019-07-12 22:03:34 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  my_family = ["Demarcus", "Diego", "Bruce", baby]
+  puppies = ["bulldog", "terrier", "poodle", "pitbull"]
 end
 
 def add_element_to_end_of_array(array, element)
