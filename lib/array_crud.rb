ff4274cc8b0e0ae7aab1144970341c59b70ffbea
diegojctorres <diegojctorres@gmail.com>
2019-07-12 22:21:46 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(create_an_array, element)
   array = ["I", "am", "really", "learning"]
-  array.push("arrays!")
+  arrays_("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
